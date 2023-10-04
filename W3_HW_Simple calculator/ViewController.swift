b530edaa91f6371e6db8616c1f17dc9b693b7045
9wynneth <gisviandhy@student.ciputra.ac.id>
2023-10-04 14:10:18 +0700

    logic updated

--- W3_HW_Simple calculator/ViewController.swift
+++ W3_HW_Simple calculator/ViewController.swift
@@ -8,151 +8,126 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var mathProc=true
+    var currentNumber = "0"
+    var currentResult: Double = 0
+    var currentOperator = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+        resetCalc()
     }
     
     @IBOutlet weak var lbl_display: UILabel!
     
-    var mathProc=true
-    
-    func AddNumberToInput(number:String){
-          var textNumber = String(lbl_display.text!)
-            if mathProc == true  {
-                textNumber = ""
-                mathProc = false
-            }
-            textNumber = textNumber + number
-            lbl_display.text = textNumber
-    }
-    @IBAction func btn0(_ sender: Any) {
-        AddNumberToInput(number: "0")
-    }
-    @IBAction func btn1(_ sender: Any) {
-        AddNumberToInput(number: "1")
-    }
-    @IBAction func btn2(_ sender: Any) {
-        AddNumberToInput(number: "2")
-    }
-    @IBAction func btn3(_ sender: Any) {
-        AddNumberToInput(number: "3")
-    }
-    @IBAction func btn4(_ sender: Any) {
-        AddNumberToInput(number: "4")
-    }
-    
-    @IBAction func btn5(_ sender: Any) {
-        AddNumberToInput(number: "5")
-    }
-    
-    @IBAction func btn6(_ sender: Any) {
-        AddNumberToInput(number: "6")
-    }
-    @IBAction func btn7(_ sender: Any) {
-        AddNumberToInput(number: "7")
-    }
-    
-    @IBAction func btn8(_ sender: Any) {
-        AddNumberToInput(number: "8")
-    }
-    @IBAction func btn9(_ sender: Any) {
-        AddNumberToInput(number: "9")
-    }
-    
-    @IBAction func btnDot(_ sender: Any) {
-        AddNumberToInput(number: ".")
-    }
-    
-    @IBAction func btnAC(_ sender: Any) {
-        lbl_display.text = "0"
+    func resetCalc() {
+        lbl_display.text="0"
         mathProc = true
+        currentNumber = "0"
+        currentResult = 0
+        currentOperator = ""
     }
     
-    
-    
-    var calcOperator = ""
-    var number1:Double?
-    
-    @IBAction func btnAdd(_ sender: Any) {
-        calcOperator = "+"
-        number1 = Double(lbl_display.text!)
-        mathProc = true
-    }
-    
-    @IBAction func btnMin(_ sender: Any) {
-        calcOperator = "-"
-        number1 = Double(lbl_display.text!)
-        mathProc = true
-    }
-    
-    @IBAction func btnMultiply(_ sender: Any) {
-        calcOperator = "*"
-        number1 = Double(lbl_display.text!)
-        mathProc = true
-    }
-    
-    @IBAction func btnDiv(_ sender: Any) {
-        calcOperator = "/"
-        number1 = Double(lbl_display.text!)
-        mathProc = true
-    }
-    
-    @IBAction func btnResult(_ sender: Any) {
-        let number2 = Double(lbl_display.text!)
-        var results:Double?
-        
-        if calcOperator == "+" {
-            results = number1! + number2!
-            lbl_display.text = String(results!)
-            mathProc = true
-        }
-        else if calcOperator == "-" {
-            results = number1! - number2!
-            lbl_display.text = String(results!)
-            mathProc = true
-        }
-        else if calcOperator == "*" {
-            results = number1! * number2!
-            lbl_display.text = String(results!)
-            mathProc = true
-        }
-        else if calcOperator == "/" {
-            results = number1! / number2!
-            lbl_display.text = String(results!)
-            mathProc = true
-        }
-        
-        if results?.truncatingRemainder(dividingBy: 1) == 0 {
-                lbl_display.text = String(Int(results!))
+    func updateLabel() {
+        let number = Double(currentNumber) ?? 0
+            if number == Double(Int(number)) {
+                lbl_display.text = String(Int(number))
             } else {
-                // If not a whole number, display as a double
-                lbl_display.text = String(results!)
+                lbl_display.text = String(number)
             }
-
-            mathProc = true
+        
     }
     
-    @IBAction func btnPerc(_ sender: Any) {
-        var number1 = Double(lbl_display.text!)
-        number1 = number1!/100.0
-        lbl_display.text = String(number1!)
+    @IBAction func AddNum(_ sender: UIButton) {
+        let num = sender.titleLabel?.text ?? ""
+        if mathProc || currentNumber == "0" {
+            currentNumber = num
+            mathProc = false
+        } else {
+            currentNumber += num
+            }
+        updateLabel()
+    }
+    
+
+    func logic() {
+        guard !currentOperator.isEmpty else { return }
+        
+        let number = Double(currentNumber) ?? 0
+        let isNeg = currentNumber.hasPrefix("-")
+        
+        if currentOperator == "+" {
+            currentResult += (isNeg ? -number : number)
+        }
+        else if currentOperator == "-" {
+            currentResult -= (isNeg ? -number : number)
+        }
+        else if currentOperator == "X" {
+            currentResult *= number
+        }
+        else if currentOperator == "/" {
+            if number != 0 {
+                currentResult /= number
+            } else {
+                lbl_display.text = "Error"
+                return
+            }
+        }
+        currentNumber = String(currentResult)
+        updateLabel()
+    }
+    
+    @IBAction func operatorPressed (_ sender: UIButton) {
+        if currentOperator.isEmpty {
+            currentResult = Double(currentNumber) ?? 0
+        } else {
+            logic()
+        }
+        currentOperator = sender.titleLabel?.text ?? ""
         mathProc = true
     }
-    
+        
+    @IBAction func btnAC(_ sender: Any) {
+        resetCalc()
+    }
+        
+    @IBAction func btnResult(_ sender: Any) {
+        logic()
+        currentOperator = ""
+        mathProc = true
+    }
+        
+    @IBAction func btnPerc(_ sender: Any) {
+        if let number = Double(currentNumber) {
+            currentNumber = String(number / 100.0)
+            updateLabel()
+            mathProc = true
+        }
+    }
+        
     @IBAction func btnMinPlus(_ sender: Any) {
-        if var number1 = lbl_display.text {
-                if number1.first != "-" {
-                    number1.insert("-", at: number1.startIndex)
-                    lbl_display.text = number1
-                } else {
-                    number1.remove(at: number1.startIndex)
-                    lbl_display.text = number1
+        if currentNumber != "0" {
+            if currentNumber.hasPrefix("-") {
+                currentNumber.removeFirst()
+            } else {
+                currentNumber = "-" + currentNumber
                 }
-            }
+            updateLabel()
+        }
     }
     
+    @IBAction func btnClearLast(_ sender: Any) {
+        if !currentNumber.isEmpty {
+            currentNumber.removeLast()
+            if currentNumber.isEmpty || currentNumber == "-" {
+                currentNumber = "0"
+            }
+            updateLabel()
+        }
+    }
+
 }
+
+
 
